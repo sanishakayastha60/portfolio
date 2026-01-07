9ce928bf5793136dd06272557e9cdf37c5513147
Sanisha <weidade003@gmail.com>
2026-01-07 09:04:45 -0800

    About Me

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import AboutMe from "./components/AboutMe";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="left-container">
+        <img src="" alt="me" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="right-container">
+        <AboutMe/>
+        <div>Skills and Technology</div>
+        <div>Featured Projects</div>
+        <div>Get In Touch</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
+export default App;
---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,11 @@
+function AboutMe() {
+    return (
+        <div>
+            <div className="title">About Me</div>
+            <div className="information">
+                <p>Hi! I am Sanisha Kayastha Shrestha and I am a creative developer passionate about building beautiful and functional digital experiences. I love combining design thinking with technical expertise to create meaningful solutions</p>
+            </div>
+        </div>
+    );
+}
+export default AboutMe;
